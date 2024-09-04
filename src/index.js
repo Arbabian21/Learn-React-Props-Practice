79ac75756b9f18180f79c6c9e991785e5f46b063
Arbabian21 <arifarbab21@gmail.com>
2024-09-04 17:12:26 -0400

    added Avatar and Detail component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById("root")
+);
 
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
